7d92e9db0c06e78d460bd94aaeea8ec401941838
Simon Lykke Andersen <simon.lykke.andersen@gmail.com>
2019-11-26 01:51:21 +0100

    Stack done (ikke testet), mangler at implementerer sammenhængen mellem stack og p5
    Stacken skulle være lavet nu.
    I sammenhæng med dette er der lavet en søster interpreter som skulle (igen det er endnu ikke testet) kunne kører ting på stacken

--- bf.js
+++ bf.js
@@ -3,7 +3,7 @@ class Interpreter{
 		this.tape = new Uint8Array(30000);
 		this.tapeIndex = 0;
 		//En regex der fjerner alt undtagen . , [ ] < > + - : ;
-		this.program = program.replace(/[^\.\,\[\]\<\>\+\-\:\;]/gi,"");
+		this.program = program.replace(/[^\.\,\[\]\<\>\+\-]/gi,"");
 		this.programIndex = 0;
 		this.loopStart = [];
 		this.output = "";
@@ -15,8 +15,8 @@ class Interpreter{
 			//console.log(char);
 			switch(char) {
 				case "<":
-				  this.left();
-				  break;
+					this.left();
+					break;
 				case ">":
 					this.right();
 					break;
@@ -37,12 +37,6 @@ class Interpreter{
 					break;
 				case "]":
 					this.loopClosing();
-					break;
-				case ":":
-					this.pushOPC();
-					break;
-				case ";":
-					this.pushARG();
 					break;
 				default:
 			  }
@@ -85,8 +79,6 @@ class Interpreter{
 			this.programIndex = this.loopStart[this.loopStart.length-1];
 		}
 	}
-	pushOPC(){}
-	pushARG(){}
 	loopEnd(index){
 		for (let iadd=0;iadd+index<this.program.length;iadd++){
 			if (this.program[index+iadd]=="]"){
@@ -96,5 +88,64 @@ class Interpreter{
 			}
 		
 	}
+	}
+}
+
+
+class InterpreterWithStack extends Interpreter{
+	constructor(program){
+		//En regex der fjerner alt undtagen . , [ ] < > + - : ;
+		super.program = program.replace(/[^\.\,\[\]\<\>\+\-\:\;\?]/gi,"");
+		this.stack = new Stack();
+	}
+	eval(){
+		for(;this.programIndex < this.program.length; this.programIndex++){
+			let char = this.program[this.programIndex];
+			//console.log(char);
+			switch(char) {
+				case "<":
+					super.left();
+					break;
+				case ">":
+					super.right();
+					break;
+				case "+":
+					super.plus();
+					break;
+				case "-":
+					super.minus();
+					break;
+				case ".":
+					super.printChar();
+					break;
+				case ",":
+					super.getChar();
+					break;
+				case "[":
+					super.loopOpening();
+					break;
+				case "]":
+					super.loopClosing();
+					break;
+				case ":":
+					this.pushOPC();
+					break;
+				case ";":
+					this.pushARG();
+					break;
+				case "?":
+					this.popARG();
+				default:
+			  }
+		}
+	}
+	pushOPC(){
+		this.stack.pushOpc(this.tape[this.tapeIndex]);
+	}
+	pushARG(){
+		this.stack.pushArg(this.tape[this.tapeIndex]);
+	}
+	popARG(){
+		this.tape[this.tapeIndex] = this.stack.popArg();
 	}
 }
--- stack.js
+++ stack.js
@@ -1,46 +1,85 @@
 class Operation{
-    constructor(func, num_of_args, returns_anything, returns_anything=null){
-        this.function = func;    
-        this.num_of_args = num_of_args;
-        //Boolean if it returns anything
-        this.returns_anything = returns_anything;
-        if(this.returns_anything){
-            this.return_type = returns_anything;
-        }
-    }
-    exec(arg_list){
-        
-    } 
+	constructor(func, num_of_args, returns_anything){
+		this.function = func;    
+		this.num_of_args = num_of_args;
+		this.returns_anything = returns_anything;
+		this.args = [];
+	}
+
+	exec(arg){
+		if (this.args.length < this.num_of_args){
+			this.args.push(arg);
+		} else if(this.args.length === this.num_of_args){
+			if (this.returns_anything){
+				return this.function(...this.args);
+			}else{
+				this.function(...this.args);
+		}} else if(true){
+			throw "Exceeded arguments";
+		}
+		//Så vi ved hvis den er nul, så har funktionen ikke fået det nødvendige antal argumenter og er dermed ikke blevet eksikveret
+		return null;
+
+	}
 }
 
-class Argument{
-    constructor(value){
-        this.value = value;
-    }
+class TempStringOperation extends Operation{
+	constructor(){
+		//En tom funktion fordi dette er et speciel case vi håndtere i exec metoden
+		super.returns_anything = true;
+		this.s = "";
+	}
+	exec(arg){
+		if (arg == 0){
+			//Den tidligere streng 
+			let previous_s = this.s;
+			//Vi "genstarter" streng værdien når den skal returneres
+			this.s = "";
+			return this.s;
+		}else{
+			this.s.concat(String.fromCharCode(arg));
+		}
+		return null;
+	}
 }
 
 let operations = {0: new Operation(resizeCanvas, 2, false),
-                  1: new Operation(fill, 1, false),
-                  2: new Operation(fill, 3, false,),
-                  3: new Operation(background, 1, false),
-                  4: new Operation(background, 3, false),
-                  5: new Operation(rect, 4, false),
-                  6: new Operation(circle, 3, false),
-                  7: new Operation(line, 4, false),
-                  8: new Operation(()=>keyIsPressed, 0, true, SOME_RETURN_TYPE),
-                  9: new Operation(()=>key, 0, true, SOME_RETURN_TYPE),
-                  10: new Operation(()=>keyCode, 0, true, SOME_RETURN_TYPE),
-                  11: new Operation(()=>mouseIsPressed, 0, true, SOME_RETURN_TYPE),
-                  12: new Operation(()=>mouseX, 0, true, SOME_RETURN_TYPE),
-                  13: new Operation(()=>mouseY, 0, true, SOME_RETURN_TYPE),
-                  14: new Operation((a, b)=>a*b, 2, true, SOME_RETURN_TYPE),
-                  15: new Operation((a, b)=>a/b, 2, true, SOME_RETURN_TYPE),
-                  16: new Operation((a, b)=>a+b, 2, true, SOME_RETURN_TYPE),
-                  17: new Operation((a, b)=>a-b, 2, true, SOME_RETURN_TYPE)}
+				  1: new Operation(fill, 1, false),
+				  2: new Operation(fill, 3, false,),
+				  3: new Operation(background, 1, false),
+				  4: new Operation(background, 3, false),
+				  5: new Operation(rect, 4, false),
+				  6: new Operation(circle, 3, false),
+				  7: new Operation(line, 4, false),
+				  8: new Operation(text, 3, false),
+				  9: new Operation(()=>keyIsPressed, 0, true),
+				  10: new Operation(()=>key, 0, true),
+				  11: new Operation(()=>keyCode, 0, true),
+				  12: new Operation(()=>mouseIsPressed, 0, true),
+				  13: new Operation(()=>mouseX, 0, true),
+				  14: new Operation(()=>mouseY, 0, true),
+				  15: new Operation((a, b)=>a*b, 2, true),
+				  16: new Operation((a, b)=>a/b, 2, true),
+				  17: new Operation((a, b)=>a+b, 2, true),
+				  18: new Operation((a, b)=>a-b, 2, true),
+				  19: new TempStringOperation()}//something with the tempstring, true)}		
 
 class Stack{
-    constructor(){
-        this.stack = [];
-    }
-
+	constructor(){
+		this.stack = [];
+		this.lastOpcI = null;
+	}
+	pushOpc(opc){
+		this.stack.push(operations[opc]);
+	}
+	pushArg(arg){
+		if (this.stack[-1].returns_anything){
+			let returned = this.stack[-1].exec(arg);
+			if (returned !== null){
+				this.pushArg(returned);
+			}
+		}else{
+			this.stack[-1].exec(arg);
+		}
+	}
 }
